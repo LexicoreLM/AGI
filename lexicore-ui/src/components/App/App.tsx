e2b16f85c3be5962ac98e58b154223b75c68873f
Denys Kuzin <denis@bodyman.io>
2024-09-09 13:06:41 +0200

    auth0 added

--- lexicore-ui/src/components/App/App.tsx
+++ lexicore-ui/src/components/App/App.tsx
@@ -3,17 +3,24 @@ import './App.css';
 import Navbar from "../Navbar";
 import ChatWindow from "../ChatWindow";
 import TasksView from "../TasksView";
+import { useAuth0 } from '@auth0/auth0-react';
 
 function App() {
-  return (
-    <div className="app">
-        {/*<Navbar />*/}
-        <div className="app__content">
-            <TasksView />
-            <ChatWindow />
-        </div>
-    </div>
-  );
+  const { loginWithRedirect, logout, isAuthenticated, user } = useAuth0();
+
+return !isAuthenticated ? (
+      <div className="app">
+        <button onClick={() => loginWithRedirect()}>Log In</button>
+      </div>
+      ) : (
+      <div className="app">
+        {/*<Navbar />*/ }
+      <div className="app__content">
+      <TasksView />
+      <ChatWindow />
+      </div>
+      </div>
+  )
 }
 
 export default App;
